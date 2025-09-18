055912caa81fe1aa88527ed242f4605806ceaa9f
seleniassDeveloper <seleniasanchez@MacBook-Air-de-SELENIA.local>
2025-09-18 13:09:44 -0500

    pruebas de push

--- Dinosaurios/eslint.config.js
+++ Dinosaurios/eslint.config.js
@@ -1,12 +1,11 @@
 // vite.config.js
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: "/", 
   plugins: [react()],
+  root: '.', // importante: raíz es el proyecto
   build: {
-    outDir: "dist",
-    emptyOutDir: true,
-  },
-});
+    outDir: 'dist'
+  }
+})
