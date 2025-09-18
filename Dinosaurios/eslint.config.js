f626815f69b0db1fcbd230d900b15d0afbb5c4a8
seleniassDeveloper <seleniasanchez@MacBook-Air-de-SELENIA.local>
2025-09-18 13:54:33 -0500

    Revert "para version"
    This reverts commit da4146b96667cf6a18d6a9494cc12f8cd9cf6a9e.

--- Dinosaurios/eslint.config.js
+++ Dinosaurios/eslint.config.js
@@ -1,12 +1,29 @@
-// vite.config.js
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import js from '@eslint/js'
+import globals from 'globals'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import { defineConfig, globalIgnores } from 'eslint/config'
 
-export default defineConfig({
-  base: "/", 
-  plugins: [react()],
-  build: {
-    outDir: "dist",
-    emptyOutDir: true,
+export default defineConfig([
+  globalIgnores(['dist']),
+  {
+    files: ['**/*.{js,jsx}'],
+    extends: [
+      js.configs.recommended,
+      reactHooks.configs['recommended-latest'],
+      reactRefresh.configs.vite,
+    ],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        ecmaFeatures: { jsx: true },
+        sourceType: 'module',
+      },
+    },
+    rules: {
+      'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
+    },
   },
-});
+])
